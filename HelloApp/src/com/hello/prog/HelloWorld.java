95f3437a8b885d1045cd3dc1b0087ed810727979
ButtiDattu <dattatreya.butti06@gmail.com>
2020-06-14 19:57:57 -0400

    updated code

--- HelloApp/src/com/hello/prog/HelloWorld.java
+++ HelloApp/src/com/hello/prog/HelloWorld.java
@@ -4,8 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("hello...........");
-		System.out.println("adding ");
-		System.out.println("delete ");
+		System.out.println("adding");
+		System.out.println("delete");
 	}
 
 }
